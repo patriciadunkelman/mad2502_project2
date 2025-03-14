216b55dbd39533d8586e7f84d0d4c94419494ac4
Conner <connerross@ufl.edu>
2025-03-14 01:03:55 -0400

    Finished the julia set method with annotations and added annotations to get_escape_time_color_arr method

--- mandelbrot.py
+++ mandelbrot.py
@@ -63,28 +63,48 @@ def get_escape_time_color_arr(
     Returns:
         np.ndarray: Array of floats determined by the escape time of each c-value
     """
-    color_arr = np.zeros(c_arr.shape, dtype =np.float64)
+    color_arr = np.zeros(c_arr.shape, dtype =np.float64) # create array with same shape as c_arr
 
-    for index, val in np.ndenumerate(c_arr):
+    for index, val in np.ndenumerate(c_arr): # iterate through each point on the grid
         et = get_escape_time(val, max_iterations)
-        if et is None:
-            color_arr[index] = 0.0
-        else:
-            color_arr[index] = (max_iterations - et + 1) / (max_iterations + 1)
-    return color_arr
+        if et is None: #Checks if the point did not escape
+            color_arr[index] = 0.0 # Set the point to black
+        else: # if point does escape, do the following
+            color_arr[index] = (max_iterations - et + 1) / (max_iterations + 1) # Use formula to calculate color
+    return color_arr #return final array
 
 
 def get_julia_color_arr(
     c_arr: np.ndarray,
+    constant: complex,
     max_iterations: int
 ) -> np.ndarray:
     """
-    Function: The function will return an array of the same shape as c_arr with color values in [0,1] according to the escape time of each c-value.
+    Function: The function uses the same logic as the Mandelbrot functions, but uses the Julia iterative formula to return a Julia Set.
 
     Args:
         c_arr (np.ndarray): Array of complex numbers
+        constant (complex): A constant complex number to be used in every iteration
         max_iterations (int): Maximum number of iterations before None is returned
 
     Returns:
         np.ndarray: Array of floats determined by the escape time of each c-value
     """
+
+    color_arr = np.zeros(c_arr.shape, dtype =np.float64) # creates zeros array with shape of c_arr
+    for index, val in np.ndenumerate(c_arr): # iterates through each point on the grid
+        z = val # set initial value of z to the current complex value
+
+        for et in range(1, max_iterations + 1): # Loop to determine escape times
+            if abs(z) > 2: #Check if magnitude is greater than 2, if so, then it escapes
+                color_arr[index] = (max_iterations - et + 1) / (max_iterations + 1) # Use formula to calculate color
+                break #exit loop when escape time is reached
+            z = z**2 + constant #update z using Julia set formula
+    return color_arr # return final array
+
+
+
+
+
+
+
